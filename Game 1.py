cff319037f799b3a6f44ab2e8b9550e6c3c14f14
GooseAkukaracha <103554799+GooseAkukaracha@users.noreply.github.com>
2022-09-27 16:14:14 +0300

    upgrade game1

--- Game 1.py
+++ Game 1.py
@@ -3,17 +3,82 @@ import random
 count = 0
 lives = 3
 
+print('''
+Вам предстоит сыграть в игру. 
+Правила просты: Вам нужно выбрать математическую операцию, затем компьютер сгенерирует пример, на который Вам нужно дать правильный ответ.
+На это у Вас есть в наличии 3 жизни. 
+С каждым неправильным ответом жизни убавляются. 
+В конце счетчик выдаст Вам количество правильно решенных примеров.
+''')
+
+print('Выберите операцию и напишите правильный ответ!')
+
+print('Список операций: Сложение, Вычитание, Деление, Умножение, Степень')
+
+sumi1 = 'Сложение'
+razn1 = 'Вычитание'
+delen1 = 'Деление'
+umnozh1 = 'Умножение'
+stepen1 = 'Степень'
+
 while lives > 0:
-    a = random.randint(-100, 100)
-    b = random.randint(-100, 100)
+    a = random.randint(-10, 10)
+    b = random.randint(-10, 10)
+    choice = input('Введите операцию: ')
+
     sumi = a + b
+    razn = a - b
+    delen = a // b
+    umnozh = a * b
+    stepen = a ** b
 
-    answ = int(input(f'Введите ответ: {a} + {b} = '))
-    if answ == sumi:
-        print('Правильный ответ!')
-        count += 1
+    if choice == sumi1:
+        answ = float(input(f'Введите ответ: {a} + {b} = '))
+        if answ == sumi:
+            print('Правильный ответ!')
+            count += 1
+        else:
+            lives -= 1
+            print(f'Неправильный ответ! Жизней: {lives}')
+
+    elif choice == razn1:
+        answ = float(input(f'Введите ответ: {a} - {b} = '))
+        if answ == razn:
+            print('Правильный ответ!')
+            count += 1
+        else:
+            lives -= 1
+            print(f'Неправильный ответ! Жизней: {lives}')
+
+    elif choice == delen1:
+        answ = float(input(f'Введите ответ: {a} // {b} = '))
+        if answ == delen:
+            print('Правильный ответ!')
+            count += 1
+        else:
+            lives -= 1
+            print(f'Неправильный ответ! Жизней: {lives}')
+
+    elif choice == umnozh1:
+        answ = float(input(f'Введите ответ: {a} * {b} = '))
+        if answ == umnozh:
+            print('Правильный ответ!')
+            count += 1
+        else:
+            lives -= 1
+            print(f'Неправильный ответ! Жизней: {lives}')
+
+    elif choice == stepen1:
+        answ = float(input(f'Введите ответ: {a} ^ {b} = '))
+        if answ == stepen:
+            print('Правильный ответ!')
+            count += 1
+        else:
+            lives -= 1
+            print(f'Неправильный ответ! Жизней: {lives}')
+
     else:
-        lives -= 1
-        print(f'Неправильный ответ! Жизней: {lives}')
+        print('Недопустимая операция! Введите еще раз.')
 
-print(f'Игра закончилась! Жизни кончились. Правильно решенных примеров {count}.')
+
+print(f'Игра закончилась! Жизни кончились. Правильно решенных примеров: {count}.')
---
+++ Module 4/Task 12.py
@@ -0,0 +1,7 @@
+num1 = int(input())
+num2 = int(input())
+
+if (1 <= num1 < 12) and (1 <= num2 < 31):
+    print(f'{num1+1} - {num2} - 2022')
+
+
