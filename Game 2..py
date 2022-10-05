200ea98af5563de746c41b17f3dc080519d77677
GooseAkukaracha <103554799+GooseAkukaracha@users.noreply.github.com>
2022-10-05 20:34:44 +0300

    new

--- Game 2..py
+++ Game 2..py
@@ -3,12 +3,12 @@ import random
 vin1 = 0
 vin2 = 0
 
-for i in range(0, 10000):
-    door = [0, 1, 0]
-    doors = random.choice(door)
+for i in range(0, 10000):   # Цикл на 10000 значений
+    door = [0, 1, 0]    # Вспомогательный список
+    doors = random.choice(door)    # Рандомный выбор в списке
     door.remove(doors)
     door.remove(0)
-    if doors == 1:
+    if doors == 1:  # Блок условия
         vin1 += 1
     elif door[0] == 1:
         vin2 += 1
