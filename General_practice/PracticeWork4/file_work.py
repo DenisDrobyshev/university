68c6065ba5cd5c1efc61f47bad768c69bbfa2ee2
GooseAkukaracha <103554799+GooseAkukaracha@users.noreply.github.com>
2022-11-16 02:13:21 +0300

    refresh

--- General_practice/PracticeWork4/file_work.py
+++ General_practice/PracticeWork4/file_work.py
@@ -1,15 +1,24 @@
+import string
 def read_file():
-    with open('read_file.txt', mode='r') as file1:
-        lists = [i.lower() + '\n' for i in file1.read().split()]
-        return lists
+    with open('read_file.txt', 'r') as rf:
+        ''' 
+         translate() в Python возвращает новую строку, в 
+         которой каждый символ в строке заменяется с использованием данной таблицы перевода. 
 
-def write_file(i, l):
-    with open('outread_file.txt', mode='w') as file2:
-        file2.write(f'Количество уникальных слов: ')
-        file2.write(str(i) + '\n')
-        file2.writelines(l)
+         maketrans(intab, outtab) -> создаем таблицу перевода, 
+         intab — Это строка, имеющая переводимые символы. 
+         outtab — Это строка, имеющая перевод intab символов. 
+         Т.е intab - это симвов, outtab - перевод 
+         '''
+        string = str.maketrans('''!()-[]{};?@#$%:'"\,./^&amp;*_''', '                             ')
+        read_f = rf.read().lower().translate(string).split()
+        return [i + '\n' for i in read_f]
 
+def write_file(count, inp):
+    with open('outread_file.txt', 'w') as wf:
+        wf.write(f'Количество уникальных слов: {str(count)}\n===============================\n')
+        wf.writelines(inp)
 
 inputs = sorted(set(read_file()))
-lens = len(inputs)
-write_file(lens, inputs)
+counts = len(inputs)
+write_file(counts, inputs)
